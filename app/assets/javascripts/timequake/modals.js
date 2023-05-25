8c06bff60c42d26df6802f1a3811b3b14a48a479
George Morris <georgemorris@Georges-MacBook-Pro-2.local>
2023-05-25 09:19:05 -0400

    remove auto tall modal

--- app/assets/javascripts/timequake/modals.js
+++ app/assets/javascripts/timequake/modals.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
   $('.modal-open').click(function(e) {
     e.preventDefault();
     let model = $(this).data('modal');
-    let needTallModal = window.innerHeight < $('.modal-content').height();
     let thisModal = $('.modal.' + model)
 
     thisModal.addClass('active');
     $('html, body').css('overflow', 'hidden');
-    needTallModal ? thisModal.addClass('modal-tall') : thisModal.removeClass('modal-tall');
   })
 
   $('.modal .modal-content').click(e => e.stopPropagation());
